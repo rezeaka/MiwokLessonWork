31b83b5da5eef3c4b82e83b0d995bab4aa6363b4
Annie Keazer-Perry <annie.keazer-perry@td.com>
2019-03-13 15:47:26 -0400

    committing to access via github to ask Jamie which approach I should actually use

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
+    /** my class training says to use this way to open the NumbersActivity */
+    public void openNumbersList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    /** I found this way to open the NumbersActivity via StackOverflow */
+    public void openNumbersList (View view){
+        //TODO: your code goes here!
+        Intent NumbersActivity = new Intent(MainActivity.this, NumbersActivity.class);
+//        myIntent.putExtra("key", value);//    <-- optional parameters
+        MainActivity.this.startActivity(NumbersActivity);
+    }
+
 }
